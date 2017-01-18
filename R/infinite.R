8aca6c0fe2a34c7abb667426aa55cea8dfa68b9a
sherrillmix <sherrillmix@users.noreply.github.com>
2017-01-18 09:28:37 -0500

    Squash bugs. Rearrange array orders

--- R/infinite.R
+++ R/infinite.R
@@ -5,27 +5,28 @@
 #' @param nBase number of bases to generate
 #' @param betweenSd cluster centers are normally distributed around 0 with this standard deviation
 #' @param withinSd points are normally distributed around cluster centers with this standard deviation
-#' @return list with matrix giving point locations (rows are points, columns are dimensions), a matrix of cluster centers and vector of cluster IDs
+#' @param nVariantBases number of bases in each PWM varying from equal proportions
+#' @return list with matrix giving point locations (rows are points, columns are dimensions), a matrix of cluster centers, a vector of sequences, a list of PWMs (rows are positions, columns are bases) and vector of cluster IDs
 #' @author Scott Sherrill-Mix \email{R@@sherrillmix.com}
 #' @export
 #' @examples
 #' clusts<-generateFakeClusters()
 #' plot(clusts$ipds,col=clusts$ids)
 #' points(clusts$centers,pch='x',col=1:nrow(clusts$centers),cex=2)
-generateFakeClusters<-function(nCluster=2,nPoints=100,nIpd=2,nBase=3,nVariantBases=min(3,nBase),betweenSd=3,withinSd=1){
+generateFakeClusters<-function(nCluster=2,nPoints=100,nIpd=6,nBase=3,nVariantBases=min(3,nBase),betweenSd=3,withinSd=1){
   centers<-lapply(1:nCluster,function(xx)stats::rnorm(nIpd,0,betweenSd))
   ipds<-mapply(function(center,n)do.call(rbind,replicate(n,stats::rnorm(length(center),center,withinSd),simplify=FALSE)),centers,nPoints,SIMPLIFY=FALSE)
   pwms<-mapply(function(x,nVar){
-    pwm<-matrix(.25,nrow=4,ncol=nBase)
+    pwm<-matrix(.25,ncol=4,nrow=nBase)
     varBases<-sample(nBase,nVar)
     for(ii in varBases){
-      pwm[,ii]<-sample(rStickBreak(4))
+      pwm[ii,]<-sample(rStickBreak(4))
     }
     return(pwm)
   },1:nCluster,nVariantBases,SIMPLIFY=FALSE)
   bases<-c('A','C','G','T')
   seqs<-apply(do.call(rbind,mapply(function(pwm,n){
-      do.call(cbind,lapply(1:nBase,function(ii)sample(bases,n,TRUE,prob=pwm[,ii])))
+      do.call(cbind,lapply(1:nBase,function(ii)sample(bases,n,TRUE,prob=pwm[ii,])))
   },pwms,nPoints,SIMPLIFY=FALSE)),1,paste,collapse='')
   return(list('ipds'=do.call(rbind,ipds),'centers'=do.call(rbind,centers),'seqs'=seqs,'pwms'=pwms,'ids'=rep(1:nCluster,sapply(ipds,nrow))))
 }
@@ -33,15 +34,15 @@ generateFakeClusters<-function(nCluster=2,nPoints=100,nIpd=2,nBase=3,nVariantBas
 #' Convert sequences to a numbered matrix
 #' @param seqs a character vector of sequences
 #' @param bases the potential characters in the sequences in the order to be numbered
-#' @return matrix with a row for each sequence and a column for each position
+#' @return 3-d logical array with the first dimension being sequence, second position and third base
 #' @author Scott Sherrill-Mix \email{R@@sherrillmix.com}
 #' @export
 #' @examples
 #' seqToMat(c('AAAT','ATCG'))
 seqToMat<-function(seqs,bases=c('A','C','G','T')){
   if(any(nchar(seqs)!=nchar(seqs[1])))stop('All sequences not same length')
-  baseLookup<-structure(1:length(bases),.Names=bases)
-  return(do.call(rbind,lapply(strsplit(seqs,''),function(xx)baseLookup[xx])))
+  seqMat<-do.call(rbind,strsplit(seqs,''))
+  return(do.call(abind::abind,c(lapply(bases,function(x)seqMat==x),list(along=3))))
 }
 
 
@@ -78,7 +79,7 @@ infiniteGaussian<-function(ipds,seqs,nIter=100,maxK=100,mc.cores=4){
   if(length(seqs)!=n)stop('Numbers of seqs does not match ipds')
   seqMat<-seqToMat(seqs)
   jagsModel <- '
-    var pwm[nSeq,4,k];
+    var pwm[k,nBase,4];
     model {
       a ~ dunif(0.3, 100)
       pwmAlpha ~ dunif(.3,100)
@@ -88,8 +89,8 @@ infiniteGaussian<-function(ipds,seqs,nIter=100,maxK=100,mc.cores=4){
           mu[ii,jj] ~ dnorm(0.0, 1)
           sigma[ii,jj] ~ dunif(0, 10)
         }
-        for(jj in 1:nSeq){
-          pwm[jj,,ii] ~ ddirch(c(pwmAlpha,pwmAlpha,pwmAlpha,pwmAlpha))
+        for(jj in 1:nBase){
+          pwm[ii,jj,] ~ ddirch(c(pwmAlpha,pwmAlpha,pwmAlpha,pwmAlpha))
         }
       }
       p[1:k] ~ ddirich(alpha[1:k])
@@ -98,8 +99,8 @@ infiniteGaussian<-function(ipds,seqs,nIter=100,maxK=100,mc.cores=4){
         for(jj in 1:nIpd){
           ipd[ii,jj] ~ dnorm(mu[z[ii],jj], sigma[z[ii],jj])
         }
-        for(jj in 1:nSeq){
-          seq[jj,,ii] ~ dmulti(pwm[jj,,z[ii]],1)
+        for(jj in 1:nBase){
+          seq[ii,jj,] ~ dmulti(pwm[z[ii],jj,],1)
         }
       }
     }
@@ -107,12 +108,13 @@ infiniteGaussian<-function(ipds,seqs,nIter=100,maxK=100,mc.cores=4){
   chains<-parallel::mclapply(sample(1:1e6,mc.cores),function(xx){
     model <- rjags::jags.model(
     textConnection(jagsModel),
-    data=list(ipd=ipds, n=n, k=maxK,nIpd=ncol(ipds),nSeq=ncol(seqMat),seq=seqMat),
+    data=list(ipd=ipds, n=n, k=maxK,nIpd=ncol(ipds),nBase=ncol(seqMat),seq=seqMat),
     inits=list(.RNG.seed=as.integer(xx),'.RNG.name'='base::Wichmann-Hill'),
     n.adapt=nIter
     )
-    chain <-rjags::coda.samples(model = jagsModel, n.iter = nIter, variable.names = c('p', 'mu', 'sigma','z','pwm','pwmAlpha','a'))
+    chain <-rjags::coda.samples(model = model, n.iter = nIter, variable.names = c('p', 'mu', 'sigma','z','pwm','pwmAlpha','a'))
     rchain <- as.matrix(chain)
     return(list('sims'=rchain,'model'=model))
   },mc.cores=mc.cores)
+  return(chains)
 }
